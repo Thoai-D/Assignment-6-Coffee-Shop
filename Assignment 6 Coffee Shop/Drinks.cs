c3398276d4d125d45f67a5d924ab583a7200c930
Thoai Do Huy <thoaidohuy@gmail.com>
2021-01-25 10:56:03 +0700

    Add project files.

---
+++ Assignment 6 Coffee Shop/Drinks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_6_Coffee_Shop
+{
+    class Drinks
+    {
+        private string name;
+        private int price;
+        private int amountsold;
+
+        public Drinks(string drinktobuy, int price)
+        {
+            name = drinktobuy;
+            this.price = price;
+        }
+
+        public string Getname()
+        {
+            return name;
+        }
+
+        public int Getprice()
+        {
+            return price;
+        }
+    }
+}
---
+++ Assignment 6 Coffee Shop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assignment_6_Coffee_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drinks[] drinks = new Drinks[5];
+
+            drinks[0] = new Drinks("Coffee", 50);
+            drinks[1] = new Drinks("Tea", 45);
+            drinks[2] = new Drinks("Juice", 30);
+            drinks[3] = new Drinks("Coffee", 50);
+
+            Console.WriteLine(drinks[0].Getname());
+
+            for (int i = 1; i < drinks.Length; i++)
+            {
+                if (drinks[i] != null)
+                {
+                    Console.WriteLine(drinks[i].Getname() + " " + drinks[i].Getprice());
+                }
+                else
+                {
+                    Console.WriteLine("Nothing is here");
+                }
+            }
+        }
+    }
